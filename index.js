3e3f7bde998f69fc2dbb23c5a3fb0a26e5c48604
blesdmm <78246748+blesdmm@users.noreply.github.com>
2026-06-04 20:47:13 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'dcdeploy20260604.blesdmm.dns-dynamic.net';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzFjNDMxNWFlN2E1MzI0MGJkNzE3MzUzNTBjMmJmYWYiLCJ0IjoiYWQ0NzE2ODEtMTRkMy00MjI4LTljZTUtYWQ4YmQyMzU3MGMxIiwicyI6Im5hQkhUZG5lcmJGVzlWUzFQeit4V1dtcGFqWWNkUEhNYnh1UVcySWxtNGZVUTNxYUE4dHZ1VGY1dVpUWUF5M3E4WnVXam8zNzVRM2hUbmdZVUt1YnBBPT0ifQ==';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
